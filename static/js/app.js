7bdfa0e30d738b7972b2d35d23d57ae9dfb960a8
Deiby Enrique Calderon <164361682+Deibycalderon777@users.noreply.github.com>
2025-09-02 17:37:11 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+
 function activeMenuOption(href) {
     $(".app-menu .nav-link")
     .removeClass("active")
@@ -83,27 +84,6 @@ app.run(["$rootScope", "$location", "$timeout", function($rootScope, $location, 
 app.controller("appCtrl", function ($scope, $http) {
 })
 app.controller("productosCtrl", function ($scope, $http) {
-    function buscarProductos() {
-        $.get("/tbodyProductos", function (trsHTML) {
-            $("#tbodyProductos").html(trsHTML)
-        })
-    }
-
-    buscarProductos()
-    
-    $(document).on("submit", "#frmProducto", function (event) {
-        event.preventDefault()
-
-        $.post("/producto", {
-            id: "",
-            nombre: $("#txtNombre").val(),
-            precio: $("#txtPrecio").val(),
-            existencias: $("#txtExistencias").val(),
-        }, function (respuesta) {
-            buscarProductos()
-        })
-    })
-
     $(document).on("click", ".btn-ingredientes", function (event) {
         const id = $(this).data("id")
 
@@ -142,9 +122,3 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     activeMenuOption(location.hash)
 })
-
-
-
-
-
-
