8265e0fc7ed4fd9bb4d5d04548cb1d4a839638f2
Deiby Enrique Calderon <164361682+Deibycalderon777@users.noreply.github.com>
2025-09-05 12:23:19 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -21,6 +21,12 @@ app.config(function ($routeProvider, $locationProvider) {
         templateUrl: "/productos",
         controller: "productosCtrl"
     })
+
+    .when("/decoraciones", {
+        templateUrl: "/decoraciones",
+        controller: "decoracionesCtrl"
+    })
+
     .when("/alumnos", {
         templateUrl: "/alumnos",
         controller: "alumnosCtrl"
@@ -57,7 +63,7 @@ app.run(["$rootScope", "$location", "$timeout", function($rootScope, $location, 
 
     $rootScope.$on("$routeChangeSuccess", function (event, current, previous) {
         $("html").css("overflow-x", "hidden")
-
+        
         const path = current.$$route.originalPath
 
         if (path.indexOf("splash") == -1) {
@@ -91,6 +97,19 @@ app.controller("productosCtrl", function ($scope, $http) {
 
     buscarProductos()
     
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true
+
+    var pusher = new Pusher("e57a8ad0a9dc2e83d9a2", {
+      cluster: "us2"
+    })
+
+    var channel = pusher.subscribe("canalProductos")
+    channel.bind("eventoProductos", function(data) {
+        // alert(JSON.stringify(data))
+        buscarProductos()
+    })
+
     $(document).on("submit", "#frmProducto", function (event) {
         event.preventDefault()
 
@@ -99,8 +118,6 @@ app.controller("productosCtrl", function ($scope, $http) {
             nombre: $("#txtNombre").val(),
             precio: $("#txtPrecio").val(),
             existencias: $("#txtExistencias").val(),
-        }, function (respuesta) {
-            buscarProductos()
         })
     })
 
@@ -116,6 +133,45 @@ app.controller("productosCtrl", function ($scope, $http) {
         })
     })
 })
+
+
+
+app.controller("decoracionesCtrl", function ($scope, $http) {
+    function buscarDecoraciones() {
+        $.get("/tbodyDecoraciones", function (trsHTML) {
+            $("#tbodyDecoraciones").html(trsHTML)
+        })
+    }
+
+    buscarDecoraciones()
+    
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true
+
+    var pusher = new Pusher("e57a8ad0a9dc2e83d9a2", {
+      cluster: "us2"
+    })
+
+    var channel = pusher.subscribe("canalDecoraciones")
+    channel.bind("eventoDecoraciones", function(data) {
+        // alert(JSON.stringify(data))
+        buscarDecoraciones()
+    })
+
+    $(document).on("submit", "#frmDecoracion", function (event) {
+        event.preventDefault()
+
+        $.post("/decoracion", {
+            id: "",
+            nombre: $("#txtNombre").val(),
+            precio: $("#txtPrecio").val(),
+            existencias: $("#txtExistencias").val(),
+        })
+    })
+})
+
+
+
 app.controller("alumnosCtrl", function ($scope, $http) {
 })
 app.controller("ventasCtrl", function ($scope, $http) {
@@ -142,5 +198,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     activeMenuOption(location.hash)
 })
+
+
+
+
+
+
+
+
+
 
 
